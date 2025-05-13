21476d21089d66071aaf9e3d7d91b9e443392f93
ngthudctun <tuanducnguyenthu@gmail.com>
2025-05-13 23:06:23 +0700

    Mai Uyên

---
+++ LoveTree-master/config.js
@@ -0,0 +1,17 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    lines: [
+        "Hôm qua",
+        "bà ngắm bông rồi",
+        "hôm nay ngắm cây nha",
+        "Với lại",
+        "tui cũng muốn cảm ơn",
+        "cảm ơn bà",
+        " ❤️ ❤️ ❤️",
+
+        
+    ],
+    
+    // date: "2017-07-20T20:15:00",
+    // names: ["笨笨熊", "黑猪猪"]
+}
